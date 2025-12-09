43f100710904e70528ce3fa78ff53c95350c38c6
ming2 <ming2@Mingders-Mac-mini.local>
2025-12-09 16:08:41 +0800

    test layout with themeswitch

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,18 @@ import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     '/Home': '/'
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
 
   adapter: vercel(),
+  integrations: [react()],
 });
